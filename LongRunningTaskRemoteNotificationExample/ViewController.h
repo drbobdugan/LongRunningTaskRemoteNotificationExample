b29d018f9be6802a7675dd3ead2e4f7fc2de75aa
drbobdugan <bdugan@stonehill.edu>
2015-11-17 13:58:30 -0500

    Initial Commit

---
+++ LongRunningTaskRemoteNotificationExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LongRunningTaskRemoteNotificationExample
+//
+//  Created by Bob Dugan on 11/17/15.
+//  Copyright © 2015 Bob Dugan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
